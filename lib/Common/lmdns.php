965520683d8a838fbeb27826c9c78e7ffd1fb3ae
liumapp <liumapp.com@gmail.com>
2017-08-15 09:50:36 +0800

    lmdns的model

--- lib/Common/lmdns.php
+++ lib/Common/lmdns.php
@@ -41,15 +41,14 @@ class lmdns
 
     public function initData(array $data)
     {
-        foreach ($data as $key => $value) {
 
+        foreach ($data as $key => $value) {
             if (property_exists( $this , $key)) {
                 $this->{$key} = $value;
             } else {
-
             }
-
         }
+
     }
 
     /**
@@ -58,6 +57,7 @@ class lmdns
      */
     public function getData (array $config)
     {
+
         $results = Capsule::table('lmdns')
             ->where('uid' , '=' , $config['uid'])
             ->where('domainId' , '=' , $config['domainId'])
@@ -74,92 +74,53 @@ class lmdns
             ->where('uid' , '=' , $config['uid'])
             ->where('domainId' , '=' , $config['domainId'])
             ->get();
-        return $result[0];
+        return $result;
     }
 
     public function select ()
     {
-        $conn = db::getInstance();
-        $queryBuilder = $conn->createQueryBuilder();
-        $result = $queryBuilder
-            ->select('*')
-            ->from($this->tableName)
-            ->where('id = ?')
-            ->setParameter(0 , $this->id)
-            ->execute();
-        $result = $result->fetch();
-        $conn->close();
-        return $result;
+        $result = Capsule::table('lmdns')
+            ->where('id' , '=' , $this->id)
+            ->get();
+        return result;
     }
 
     public function updateRecord ()
     {
-        $conn = db::getInstance();
-        $queryBuilder = $conn->createQueryBuilder();
-        $this->validate();
-        $queryBuilder
-            ->update('lmdns')
-            ->set('subdomain' , ':subdomain')
-            ->set('value' , ':value')
-            ->set('type' , ':type')
-            ->set('ipIndex' , ':ipIndex')
-            ->where('id = :id' )
-            ->setParameter(':subdomain' , $this->subdomain)
-            ->setParameter(':value' , $this->value)
-            ->setParameter(':type' , $this->type)
-            ->setParameter(':ipIndex' , $this->ipIndex)
-            ->setParameter(':id', $this->id);
-        $conn->close();
-        return $queryBuilder->execute();
+        $result = Capsule::table('lmdns')
+            ->where('id' , '=' , $this->id)
+            ->update([
+                'subdomain' => $this->subdomain,
+                'value' => $this->value,
+                'type' => $this->type,
+                'ipIndex' => $this->ipIndex,
+            ]);
+        return $result;
     }
 
     public function addRecord ()
     {
-        $conn = db::getInstance();
-        $queryBuilder = $conn->createQueryBuilder();
-        $this->validate();
-        $queryBuilder
-            ->insert('lmdns')
-            ->values(
-                array(
-                    'uid' => '?',
-                    'domainId' => '?',
-                    'type' => '?',
-                    'subdomain' => '?',
-                    'value' => '?',
-                    'ipIndex' => '?',
-                )
-            )
-            ->setParameter(0, $this->uid)
-            ->setParameter(1, $this->domainId)
-            ->setParameter(2, $this->type)
-            ->setParameter(3, $this->subdomain)
-            ->setParameter(4, $this->value)
-            ->setParameter(5, $this->ipIndex)
-        ;
+        $result = Capsule::table('lmdns')
+            ->insert([
+                'uid' => $this->uid,
+                'domainId' => $this->domainId,
+                'type' => $this->type,
+                'subdomain' => $this->subdomain,
+                'value' => $this->value,
+                'ipIndex' => $this->ipIndex,
+            ]);
 
-        $result = $queryBuilder->execute();
-        $conn->close();
         return $result;
     }
 
     public function delRecord ()
     {
-        $conn = db::getInstance();
-        $queryBuilder = $conn->createQueryBuilder();
-        $queryBuilder->delete('lmdns')
-            ->where('id = :record_id')
-            ->andWhere('uid = :uid')
-            ->andWhere('domainId = :domainId')
-            ->setParameter(':record_id', $this->id)
-            ->setParameter(':uid' , $this->uid)
-            ->setParameter(':domainId' , $this->domainId)
-            ->execute();
-
-        $result = $queryBuilder->execute();
-        $conn->close();
+        $result = Capsule::table('lmdns')
+            ->where('id' , '=' , $this->id)
+            ->where('uid' , '=' , $this->uid)
+            ->where('domainId' , '=' , $this->domainId)
+            ->delete();
         return $result;
-
     }
 
     public function ReloadIpIndex ($uid , $domainId , $type)
@@ -178,38 +139,23 @@ class lmdns
 
     public function getNewIndex ()
     {
-        $conn = db::getInstance();
-        $queryBuilder = $conn->createQueryBuilder();
-        $result = $queryBuilder
-             ->select('ipIndex')
-             ->from($this->tableName)
-             ->where('uid = ? and domainId = ? and type = ?')
-             ->orderBy('ipIndex' , 'DESC')
-             ->setParameter(0 , $this->uid)
-             ->setParameter(1 , $this->domainId)
-             ->setParameter(2 , $this->type)
-             ->execute();
-        $results = $result->fetch();
-        $conn->close();
-        if (!isset($results['ipIndex'])) {
+        $result = Capsule::table('lmdns')
+            ->select('ipIndex')
+            ->where('uid' , '=' , $this->uid)
+            ->where('domainId' , '=' , $this->domainId)
+            ->where('type' , '=' , $this->type)
+            ->get();
+        if (!isset($result['ipIndex'])) {
             return 1;
         } else {
-            return $results['ipIndex'] + 1;
+            return $result['ipIndex'] + 1;
         }
     }
 
     public function getNewRecordId ()
     {
-        $conn = db::getInstance();
-        $sql = "SELECT LAST_INSERT_ID()";
-
-        $stmt = $conn->query($sql); // Simple, but has several drawbacks
-
-        $result = $stmt->fetchColumn(0);
-
-        $conn->close();
-
-        return $result;//返回id
+        $result = Capsule::select('SELECT LAST_INSERT_ID()');
+        return $result;
     }
 
     public function validate ()
